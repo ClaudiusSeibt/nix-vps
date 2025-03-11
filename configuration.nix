ab5f2a665765ee91efce546c5740cfc3454fd1d8
ClaudiusSeibt <Claudius.seibt@gmail.com>
2025-03-12 00:20:36 +0100

    improved openssh and added ssh-sk keys

--- configuration.nix
+++ configuration.nix
@@ -3,29 +3,34 @@
   lib,
   pkgs,
   ...
-}:
-{
+}: {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
     (modulesPath + "/profiles/qemu-guest.nix")
     ./disk-config.nix
   ];
   boot.loader.grub = {
-    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
-    # devices = [ ];
     efiSupport = true;
     efiInstallAsRemovable = true;
   };
-  services.openssh.enable = true;
+  services = {
+    openssh = {
+      enable = true;
+      settings.PasswordAuthentication = false;
+      settings.KbdInteractiveAuthentication = false;
+      settings.PermitRootLogin = "yes";
+    };
+  };
 
   environment.systemPackages = map lib.lowPrio [
     pkgs.curl
     pkgs.gitMinimal
-    pkgs.kittysay
   ];
 
   users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAjKs7qQSPMEruRDLTTyiWyvBBiiLchYgMMhwQEz4h+r claudi@MBA.local"
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIIkNbmX41jlVM8wEF+FcIK8nuUGq+TnwRXk1n8IkeUPoAAAABHNzaDo= yk0"
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDSocOkGTFJhwoRq6CM3nzy5WmrbYAR5mYWGJjdgFt+BAAAABHNzaDo= t0"
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAICizxnc24n8ipxskoKP//erIfGVxwxTW9jtzLWR0KVoxAAAABHNzaDo= t1"
   ];
 
   system.stateVersion = "24.05";
